fe2adc47033d1ebb18dfdf4e56b2cafc1df033a7
Pithak-00 <31059244+Pithak-00@users.noreply.github.com>
2022-10-18 17:51:16 +0900

    弓に当たり判定追加

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    [SerializeField] float arrowSpeed = 20f;
+    Rigidbody2D myRigidBody;
+    [SerializeField] GameObject player;
+    PlayerMovement playerMovement;
+    float xSpeed;
+
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        playerMovement = player.GetComponent<PlayerMovement>();
+        xSpeed = playerMovement.transform.localScale.x * arrowSpeed;
+    }
+
+    void Update()
+    {
+        myRigidBody.velocity = new Vector2(xSpeed, 0f);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+        }
+        Destroy(gameObject);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
